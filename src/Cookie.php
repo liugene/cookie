c9697a86f619178339949efa5e815fb73de7ed5d
liugene <750688237@qq.com>
2018-06-11 21:28:38 +0800

    初始化

---
+++ src/Cookie.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace linkphp\cookie;
+
+
+class Cookie {
+    /**
+     * COOKIE配置
+     */
+    protected $config = array();
+    /**
+     * @param string $config['path'] 路径
+     * @param string $config['domain'] 域名
+     * @param boolean $config['secure'] 是否加密
+     * @param boolean $config['httponly'] 是否只HTTP协议
+     */
+    public function __construct($config = array()) {
+        $this->config['path']       = isset($config['path']) ? $config['path'] : NULL;
+        $this->config['domain']     = isset($config['domain']) ? $config['domain'] : NULL;
+        $this->config['secure']     = isset($config['secure']) ? $config['secure'] : FALSE;
+        $this->config['httponly']   = isset($config['httponly']) ? $config['httponly'] : FALSE;
+    }
+    /**
+     * 获取COOKIE
+     *
+     * @param string $name 待获取的COOKIE名字
+     * @return string/NULL/array $name为NULL时返回整个$_COOKIE，存在时返回COOKIE，否则返回NULL
+     */
+    public function get($name = NULL) {
+        if ($name === NULL) {
+            return $_COOKIE;
+        }
+        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : NULL;
+    }
+    /**
+     * 设置COOKIE
+     *
+     * @param string $name 待设置的COOKIE名字
+     * @param string/int $value 建议COOKIE值为一些简单的字符串或数字，不推荐存放敏感数据
+     * @param int $expire 有效期的timestamp，为NULL时默认存放一个月
+     * @param boolean
+     * @return bool
+     */
+    public function set($name, $value, $expire = NULL) {
+        if ($expire === NULL) {
+            $expire = $_SERVER['REQUEST_TIME'] + 2592000;   //a month
+        }
+        return setcookie(
+            $name,
+            $value,
+            $expire,
+            $this->config['path'],
+            $this->config['domain'],
+            $this->config['secure'],
+            $this->config['httponly']
+        );
+    }
+    /**
+     * 删除COOKIE
+     *
+     * @param strint $name 待删除的COOKIE名字
+     * @param boolean
+     * @see Cookie::set()
+     * @return bool
+     */
+    public function delete($name) {
+        return $this->set($name, '', 0);
+    }
+    /**
+     * 获取COOKIE的配置
+     */
+    public function getConfig() {
+        return $this->config;
+    }
+}
